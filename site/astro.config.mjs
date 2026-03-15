180c0129eecfc4b11e989671601dee8349858c67
Erik Uus <erik.uus@gmail.com>
2026-03-15 18:30:25 +0200

    Fix styling after custom domain

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
-const isCi = process.env.GITHUB_ACTIONS === "true";
-
 export default defineConfig({
-  site: "https://erikuus.github.io",
-  base: isCi ? "/undo-uus-libertarian-imperative" : "/",
+  site: "https://libertarianimperative.org",
   output: "static",
   integrations: [mdx()],
   markdown: {
